3f1fa66987bf56a2de86928f844a8712725f803a
oscarartieda8-cloud <oscarartieda8@gmail.com>
2026-04-20 15:58:16 -0500

    clase de Javascript 1

---
+++ 20_04_2026 JS/script.js
@@ -0,0 +1,25 @@
+/*function saludar(){
+    console.log('hola');
+}
+saludar(); */
+
+
+/*const insertarEstudiante = () => {
+    const titulo = document.getElementById("principal");
+    console.log(principal);
+}
+
+insertarEstudiante();
+
+const ponerColorBg = (color) =>{
+    const elemento = document.getElementById("principal");
+    elemento.style.backgroundColor="gainsboro"
+    elemento.innerText = "AMONGUS!"
+}*/
+
+const agregarEstudiante = () => {
+    const contenedor = document.getElementById("contenedor");
+    const itemEstudiante = document.createElement("p");
+    itemEstudiante.innerText="HOLAAAAAAAAAAA";
+    contenedor.appendChild(itemEstudiante);
+}
