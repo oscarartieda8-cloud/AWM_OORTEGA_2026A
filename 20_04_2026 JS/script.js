e2f824240df02bb6e0d0e08b209dd149e900aeaa
oscarartieda8-cloud <oscarartieda8@gmail.com>
2026-04-22 16:01:50 -0500

    JAVASCRIPT

--- 20_04_2026 JS/script.js
+++ 20_04_2026 JS/script.js
@@ -16,10 +16,42 @@ const ponerColorBg = (color) =>{
     elemento.style.backgroundColor="gainsboro"
     elemento.innerText = "AMONGUS!"
 }*/
-
+/*
 const agregarEstudiante = () => {
     const contenedor = document.getElementById("contenedor");
     const itemEstudiante = document.createElement("p");
     itemEstudiante.innerText="HOLAAAAAAAAAAA";
     contenedor.appendChild(itemEstudiante);
+}
+*/
+
+/* Usar objetos
+var estudiante={id:0, nombre: "oscar_lal"};
+*/
+const lstEstudiantes = [{id:1, nombre:"Osker"},
+    {id:2, nombre:"Pacheke"},
+    {id:3, nombre: "Manuelito"}
+];
+
+ 
+const renderizarListaEstudiantes = () => {
+    const contenedor = document.getElementById("contenedor");
+    contenedor.innerText=""; //Reiniciamos la pagina 
+    lstEstudiantes.forEach((estudiante) =>{
+        const itemEstudiante = document.createElement("p");
+        itemEstudiante.innerText=estudiante.nombre;
+        contenedor.appendChild(itemEstudiante);
+    });
+}
+/*
+//llamando directamente cuando se inicia el renderizado pero tambien lo agregué al botón
+renderizarListaEstudiantes();
+*/
+
+
+const insertarEstudiante = (event) =>{
+    event.preventDefault();//para evitar que recargue la pagina
+    const nombreEstudiante = document.getElementById("nombreEstudiante").value;
+    lstEstudiantes.push({id:0, nombre: nombreEstudiante});
+    renderizarListaEstudiantes();
 }
