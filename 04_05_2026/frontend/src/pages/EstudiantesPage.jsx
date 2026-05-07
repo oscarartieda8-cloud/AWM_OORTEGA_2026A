55119139803c0e7b377a18309dfe6a4c6e92e28f
oscarartieda8-cloud <oscarartieda8@gmail.com>
2026-05-07 14:08:11 -0500

    variable de estaod y paso de datos padre a hijo

--- 04_05_2026/frontend/src/pages/EstudiantesPage.jsx
+++ 04_05_2026/frontend/src/pages/EstudiantesPage.jsx
@@ -1,10 +1,21 @@
 import Estudiante from "../components/estudiante";
+import { listaEstudiantes } from "../utils/data";
+
 const EstudiantesPage = () => {
   return (
     <div>
+      
+      {/*} DATOS HARDCODEADOS 
       <Estudiante nombre={"Amogus"} edad={40} url={"https://www.youtube.com/"} />
       <Estudiante nombre={"Pacheko"} edad={77} url={"https://www.youtube.com/"} />
       <Estudiante nombre={"Manuela"} edad={66} url={"https://www.youtube.com/"} ></Estudiante>
+      {*/}
+      {
+      listaEstudiantes.map((estudiante) => 
+        {
+          return <Estudiante nombre={estudiante.nombre} edad={estudiante.edad} url={estudiante.url}></Estudiante>
+        })
+      }
     </div>
   );
 }
--- 04_05_2026/frontend/src/utils/data.js
+++ 04_05_2026/frontend/src/utils/data.js
@@ -1,7 +1,7 @@
 export const listaEstudiantes = [
-    { id: 1, nombre: "oscar", edad: 10, url="https://www.youtube.com/" },
-    { id: 2, nombre: "el", edad: 10.1, url="https://www.youtube.com/" },
-    { id: 3, nombre: "genial", edad: 10.2, url="https://www.youtube.com/" },
-    { id: 4, nombre: "jaja", edad: 10.3, url="https://www.youtube.com/" },
-    { id: 666, nombre: "lol", edad: 10.4, url="https://www.youtube.com/" }
+    { id: 1, nombre: "oscar", edad: 10, url:"https://www.youtube.com/" },
+    { id: 2, nombre: "el", edad: 10.1, url:"https://www.youtube.com/" },
+    { id: 3, nombre: "genial", edad: 10.2, url:"https://www.youtube.com/" },
+    { id: 4, nombre: "jaja", edad: 10.3, url:"https://www.youtube.com/" },
+    { id: 666, nombre: "lol", edad: 10.4, url:"https://www.youtube.com/" }
 ]
