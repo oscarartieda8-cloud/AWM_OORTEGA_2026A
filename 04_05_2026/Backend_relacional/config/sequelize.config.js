93e00b0dfd772982e88b9832ec09dc0cb4b3ab77
oscarartieda8-cloud <oscarartieda8@gmail.com>
2026-06-10 16:01:13 -0500

    Configuracion inicial de Sequelize

---
+++ 04_05_2026/Backend_relacional/config/sequelize.config.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require("sequelize");
+const env = require("../config/env");
+
+module.exports.createSequelize = () =>{
+
+    const seq = new Sequelize(env.db.name, env.db.user, env.db.pass, {
+        host: env.db.host,
+        port: env.db.port,
+        dialect: env.db.dialect,
+        loggin: false,
+        define: {
+            underscored: true,
+            freezeTableName: true,
+            timestamps: false,
+        },
+        pool: {
+            max: 10,
+            min:0,
+            acquire: 30000,
+            idle: 10000,
+        },
+
+    });
+
+    return seq;
+
+
+}
+
