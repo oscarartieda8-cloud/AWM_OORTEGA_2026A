290f83f2742e307d743c4db4119d0eda51d3ac41
Oscar <oscar.ortega.artieda8@gmail.com>
2026-06-14 22:12:25 -0500

    Terminado el CRUD con MongoDB

--- 04_05_2026/Backend/server/controllers/estudiante.controller.js
+++ 04_05_2026/Backend/server/controllers/estudiante.controller.js
@@ -11,12 +11,27 @@ module.exports.getEstudiante = (req, res) =>{
     const {id} = req.params;
     Estudiante.findOne({_id:id})
         .then(user => res.json(user))
-        .catch(err => res.json(err))
+        .catch(err => res.status(500).json(err))
 }
 
 module.exports.postEstudiante = (req, res) => {
     const {nombre, edad, url} = req.body;
     Estudiante.create({nombre,edad,url})
-        .then(res => res.json({message: "QUE BIEN SI SE INGRESO"}))
-        .catch(err => res.json(err))
-} //algo esta mal
+        .then(resultado => res.json({message: "QUE BIEN SI SE INGRESO"}))
+        .catch(err => res.status(500).json(err))
+}
+
+module.exports.putEstudiantes = (req, res) => {
+    const {id} = req.params;
+    const {nombre, edad, url} = req.body;
+    Estudiante.updateOne({_id:id}, {nombre, edad, url})
+        .then(resultado => res.json({message: "ESTUDIANTE ACTUALIZADO"}))
+        .catch(err => res.status(500).json(err))
+}
+
+module.exports.deleteEstudiante = (req, res) => {
+    const {id} = req.params;
+    Estudiante.deleteOne({_id:id})
+        .then(resultado => res.json({message: "ESTUDIANTE ELIMINADO"}))
+        .catch(err => res.status(500).json(err))
+}
--- 04_05_2026/Backend/server/routes/estudiante.routes.js
+++ 04_05_2026/Backend/server/routes/estudiante.routes.js
@@ -3,7 +3,9 @@ const estudianteController = require('../controllers/estudiante.controller');
 module.exports = function(app){
     app.get('/estudiantes', estudianteController.getAllEstudiantes)
     app.get('/estudiantes/:id', estudianteController.getEstudiante)
-    app.post('/estudiante/nuevo', estudianteController.postEstudiante)
+    app.post('/estudiantes/nuevo', estudianteController.postEstudiante)
+    app.put('/estudiantes/editar/:id', estudianteController.putEstudiantes)
+    app.delete('/estudiantes/eliminar/:id', estudianteController.deleteEstudiante)
 }
 
 
